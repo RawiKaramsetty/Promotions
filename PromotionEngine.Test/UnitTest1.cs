800e2d43f9a99eb14c341caca1cb91d78248dd5f
RawiKaramsetty <rawikaramsetty@gmail.com>
2021-06-25 17:59:31 +0530

    Commuting few changes to Test solution

--- PromotionEngine.Test/UnitTest1.cs
+++ PromotionEngine.Test/UnitTest1.cs
@@ -1,101 +1,30 @@
-using PromotionEngine.DTO.DTOs;
-using PromotionEngine.DTO.Enums;
-using PromotionEngine.DTO.Models;
-using PromotionEngine.Repositories.IRepositories;
 using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.DTO.DTOs;
+using PromotionEngine.DTO.Models;
+using PromotionEngine.Repositories.Repositories;
 using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
-
-namespace PromotionEngine.Repositories.Repositories
+namespace PromotionEngine.Test
 {
-    public class PromotionEngineRepositery : IPromotionEngineRepositery
+    [TestClass]
+    public class PromotionEngineRepositeryTest
     {
-        public List<Promotion> GetPromotions()
+        [TestMethod]
+        public void TestMethod1()
         {
+            PromotionEngineRepositery promotionEngineRepositery = new PromotionEngineRepositery();
+
+            List<CartItem> cartItems = new List<CartItem>();
             List<Promotion> promotions = new List<Promotion>();
-            promotions.Add(new Promotion { SKUName = SKU.A.ToString(), PromotionPrice = 130, PromotionQuantity = 3, IsPromotionActive = true });
-            promotions.Add(new Promotion { SKUName = SKU.B.ToString(), PromotionPrice = 45, PromotionQuantity = 2, IsPromotionActive = true });
-            promotions.Add(new Promotion { SKUName = $"{SKU.C} & {SKU.D}", PromotionPrice = 30, PromotionQuantity = 1, IsPromotionActive = true });
-            return promotions;
-        }
-
-        public List<CartDTO> ProcessPromotion(List<CartItem> cartItems)
-        {
-            return ProcessPromotionHelper(cartItems);
-        }
-
-        public static List<CartDTO> ProcessPromotionHelper(List<CartItem> cartItems)
-        {
-            List<CartDTO> items = new List<CartDTO>();
-            CartDTO cartItem; Promotion promotion;
-
-            #region Calculate for SKU A, B
-            foreach (var item in cartItems)
+            promotions.Add(new Promotion
             {
-                cartItem = new CartDTO();
-                promotion = GetPromotions().FirstOrDefault(x => x.SKUName.Equals(item.SKUItem.ToString()));
-                if (promotion != null)
-                {
-                    switch (item.SKUItem)
-                    {
-                        case SKU.A:
-                            cartItem.SKUName = SKU.A.ToString();
-                            cartItem.ActualCost = Convert.ToInt32(SKU.A);
-                            break;
-                        case SKU.B:
-                            cartItem.SKUName = SKU.B.ToString();
-                            cartItem.ActualCost = Convert.ToInt32(SKU.B);
-                            break;
-                    }
-                    cartItem.OfferedQuantity = item.SKUQuantity / promotion.PromotionQuantity;
-                    cartItem.ActualQuantity = item.SKUQuantity;
-                    cartItem.EligibleQuantityOfferedCost = cartItem.OfferedQuantity * promotion.PromotionPrice;
-                    cartItem.FinalCost = cartItem.EligibleQuantityOfferedCost + ((item.SKUQuantity % promotion.PromotionQuantity) * cartItem.ActualCost);
-                    items.Add(cartItem);
-                }
-            }
-            #endregion
-
-
-            #region Calculating for SKU C&D combo
-            int skuC_Quantity = cartItems.Where(x => x.SKUItem.Equals(SKU.C)).Select(x => x.SKUQuantity).First();
-            int skuD_Quantity = cartItems.Where(x => x.SKUItem.Equals(SKU.D)).Select(x => x.SKUQuantity).First();
-            cartItem = new CartDTO();
-            promotion = GetPromotions().FirstOrDefault(x => x.SKUName.Equals($"{SKU.C} & {SKU.D}"));
-
-            if (promotion != null)
-            {
-                cartItem.SKUName = $"{SKU.C} & {SKU.D}";
-
-                if (skuC_Quantity > skuD_Quantity)
-                {
-                    cartItem.ActualCost = Convert.ToInt32(SKU.C);
-                    cartItem.ActualQuantity = skuC_Quantity;
-                    cartItem.OfferedQuantity = skuD_Quantity;
-                    cartItem.EligibleQuantityOfferedCost = cartItem.OfferedQuantity * promotion.PromotionPrice;
-                    cartItem.FinalCost = cartItem.EligibleQuantityOfferedCost + ((skuC_Quantity - skuD_Quantity) * cartItem.ActualCost);
-                }
-                else if (skuD_Quantity > skuC_Quantity)
-                {
-                    cartItem.ActualCost = Convert.ToInt32(SKU.D);
-                    cartItem.ActualQuantity = skuD_Quantity;
-                    cartItem.OfferedQuantity = skuC_Quantity;
-                    cartItem.EligibleQuantityOfferedCost = cartItem.OfferedQuantity * promotion.PromotionPrice;
-                    cartItem.FinalCost = cartItem.EligibleQuantityOfferedCost + ((skuD_Quantity - skuC_Quantity) * cartItem.ActualCost);
-                }
-                else
-                {
-                    cartItem.ActualQuantity = cartItem.OfferedQuantity = skuD_Quantity; // or skuC_Quantity
-                    cartItem.EligibleQuantityOfferedCost = cartItem.OfferedQuantity * promotion.PromotionPrice;
-                    cartItem.FinalCost = cartItem.EligibleQuantityOfferedCost;
-                }
-
-                items.Add(cartItem);
-            }
-            #endregion
-
-            return items;
+                IsPromotionActive = true,
+                PromotionPrice = 100,
+                PromotionQuantity = 2,
+                SKUName = "Test"
+            });
+            var result = promotionEngineRepositery.GetPromotions();
+            Assert.IsNotNull(result);
         }
     }
 }
